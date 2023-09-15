fb8c15c958e4657ee15953a6baf06fccef94bb40
RAVI RAJ LADHA <78008773+ravirajladha@users.noreply.github.com>
2023-09-15 11:50:15 +0530

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="fixed h-8 bottom-0 px-2 py-1 flex items-center justify-center w-full text-xs text-gray-500 ">
+      <span>
+       
+         Kods Technolgies Private Limited.{" "}
+      
+       
+      </span>
+    </div>
+  );
+};
+
+export default Footer;
